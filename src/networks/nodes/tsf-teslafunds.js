5b028ed551742620c3434db22932b1d4fd325835
Jan <janonorati@yahoo.it>
2019-01-23 22:24:30 +0100

    adding tsf network

---
+++ src/networks/nodes/tsf-teslafunds.js
@@ -0,0 +1,10 @@
+import { ETC } from '../types';
+export default {
+  type: TSF,
+  service: 'Teslafunds node',
+  url: 'https://tsfapi.europool.me',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+};
---
+++ src/networks/types/TSF.js
@@ -0,0 +1,18 @@
+// import tokens from '@/tokens/tokens-tsf.json';
+import contracts from '@/contracts/contract-abi-tsf.json';
+import etc from '@/assets/images/networks/tsf.svg';
+// import { EthAbi } from '../ensAbis';
+
+export default {
+  name: 'TSF',
+  name_long: 'Teslafunds',
+  homePage: 'https://teslafunds.io/',
+  blockExplorerTX: 'https://tsfexplorer.xyz/tx/[[txHash]]',
+  blockExplorerAddr: 'https://tsfexplorer.xyz/address/[[address]]',
+  chainID: 1856,
+//  tokens: tokens,
+  contracts: contracts,
+  ensResolver: '',
+  ensAbi: '',
+  icon: etc
+};
