2afb2cf71cf623e41fa49bc72a7d372bff2868e1
Jan <janonorati@yahoo.it>
2019-01-24 02:02:05 +0100

    fix address

--- src/networks/nodes/tsf.js
+++ src/networks/nodes/tsf.js
@@ -2,7 +2,7 @@ import { TSF } from '../types';
 export default {
   type: TSF,
   service: 'Teslafunds.io',
-  url: '127.0.0.1',
+  url: '192.168.1.54:',
   port: 4949,
   auth: false,
   username: '',
