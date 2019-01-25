e2c6733127aea3091e329126ab4768e824747d6c
Jan <janonorati@yahoo.it>
2019-01-25 19:04:05 +0100

    fix

--- src/networks/types/TSF.js
+++ src/networks/types/TSF.js
@@ -1,6 +1,6 @@
 import tokens from '@/tokens/tokens-tsf.json';
 import contracts from '@/contracts/contract-abi-tsf.json';
-import etc from '@/assets/images/networks/tsf.svg';
+import tsf from '@/assets/images/networks/tsf.svg';
 // import { EthAbi } from '../ensAbis';
 
 export default {
@@ -10,7 +10,7 @@ export default {
   blockExplorerTX: 'https://tsfexplorer.xyz/tx/[[txHash]]',
   blockExplorerAddr: 'https://tsfexplorer.xyz/addr/[[address]]',
   chainID: 56,
-  //tokens: tokens,
+  tokens: tokens,
   contracts: contracts,
   ensResolver: '',
   ensAbi: '',
