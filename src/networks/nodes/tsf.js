6256a85b521381a52311a7bd8a9699e5aee6667d
Jan <janonorati@yahoo.it>
2019-01-30 15:11:34 +0100

    Revert "Update tsf.js"
    This reverts commit e17694d89bed3fc3bbcdb0ca5b64e5fe7d117d6d.

--- src/networks/nodes/tsf.js
+++ src/networks/nodes/tsf.js
@@ -2,7 +2,7 @@ import { TSF } from '../types';
 export default {
   type: TSF,
   service: 'Teslafunds.io',
-  url: 'http://185.206.146.59',
+  url: 'http://94.156.189.141',
   port: 4949,
   auth: false,
   username: '',
