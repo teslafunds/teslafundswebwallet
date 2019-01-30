3f66c37544eadb6f21d5c76f863fac80a84ab6f0
Jan <janonorati@yahoo.it>
2019-01-30 10:57:05 +0100

    fix node network api

--- src/networks/nodes/tsf.js
+++ src/networks/nodes/tsf.js
@@ -2,7 +2,7 @@ import { TSF } from '../types';
 export default {
   type: TSF,
   service: 'Teslafunds.io',
-  url: 'https://tsfapi.europool.me/',
+  url: 'https://tsfapi.europool.me',
   port: 443,
   auth: false,
   username: '',
